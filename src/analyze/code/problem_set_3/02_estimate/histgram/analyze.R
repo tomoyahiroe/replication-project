9009683a2d25396eed136e62569e045ff8363967
tomoyahiroe <tomoya4creative@gmail.com>
2024-02-05 05:29:05 +0900

    fix: ヒストグラムの修正

--- src/analyze/code/problem_set_3/02_estimate/histgram/analyze.R
+++ src/analyze/code/problem_set_3/02_estimate/histgram/analyze.R
@@ -3,9 +3,9 @@
 main <- function() {
   load(basics$get_absolute_path("src/analyze/output/data/simulate_dataset.rda"))
 
-  hist1 <- plot_modules$plot_histogram(data = simulate_dataset, x = X, bin = 10, title = "bin = 1")
-  hist2 <- plot_modules$plot_histogram(data = simulate_dataset, x = X, bin = 1, title = "bin = 0.1")
-  hist3 <- plot_modules$plot_histogram(data = simulate_dataset, x = X, bin = 0.1, title = "bin = 0.01")
+  hist1 <- plot_modules$plot_histogram(data = simulate_dataset, x = X, bin = 10, title = "bin = 10")
+  hist2 <- plot_modules$plot_histogram(data = simulate_dataset, x = X, bin = 0.5, title = "bin = 0.5")
+  hist3 <- plot_modules$plot_histogram(data = simulate_dataset, x = X, bin = 0.1, title = "bin = 0.1")
 
   grid_hist <- gridExtra::grid.arrange(hist1, hist2, hist3, nrow = 2)
 
