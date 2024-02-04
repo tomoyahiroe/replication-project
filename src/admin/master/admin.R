bc42d97314060a0ca23b984478596345ffc67650
tomoyahiroe <tomoya4creative@gmail.com>
2024-02-05 08:53:45 +0900

    feat: master.R

--- src/admin/master/admin.R
+++ src/admin/master/admin.R
@@ -1,20 +1,9 @@
 main <- function() {
-  # Install packages
-  # install()
-
-  # Initialize
-  initialize()
-
+  # Install, initializeはセッション開示時に実行されるはず
   # Build data
   build()
-}
 
-install <- function() {
-  source("src/admin/packages/admin.R")
-}
-
-initialize <- function() {
-  source("src/admin/initialize/admin.R")
+  analyze()
 }
 
 build <- function() {
@@ -24,6 +13,25 @@ build <- function() {
   source("src/build/gradrate_ready/code/build.R")
   source("src/build/covariates_ready/code/build.R")
   source("src/build/master/code/build.R")
+
+  source("src/build/table_1/code/build.R")
+}
+
+analyze <- function() {
+	# Descriptive statistics
+	source("src/analyze/code/descriptive_statistics/table/analyze.R")
+	source("src/analyze/code/descriptive_statistics/4yr_gradrate_trend/analyze.R")
+	source("src/analyze/code/descriptive_statistics/semester_rate_trend/analyze.R")
+	source("src/analyze/code/descriptive_statistics/scatter_plot/analyze.R")
+
+	# Regression analysis
+	source("src/analyze/code/regression_analysis/analyze.R")
+
+	# Problem Set 3
+	source("src/analyze/code/problem_set_3/01_simulate/analyze.R")
+	source("src/analyze/code/problem_set_3/02_estimate/histgram/analyze.R")
+	source("src/analyze/code/problem_set_3/02_estimate/kdensity/analyze.R")
+	source("src/analyze/code/problem_set_3/02_estimate/quantile_regression/analyze.R")
 }
 
 main()
